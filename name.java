bf6ec2799fad15a9ac1885273da4b06690b4a6c4
bala-omkar-neteinc <bala.surampalli@netenrich.com>
2023-06-12 15:51:39 +0530

    added name and sum files

---
+++ name.java
@@ -0,0 +1,9 @@
+public class name 
+{
+    public static void main(String args[])
+    {
+        String s="Bala Omkar";
+        System.out.println(s);
+    }
+    
+}
---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum
+{
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=3;
+        int c=a+b;
+
+
+        
+        System.out.println("The sum of a and b is = "+c);
+    }
+}
