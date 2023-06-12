8a3e970aad77b4589de84490bc9b6993683129ee
bala-omkar-neteinc <bala.surampalli@netenrich.com>
2023-06-12 16:06:34 +0530

    Full name

--- name.java
+++ name.java
@@ -2,7 +2,7 @@ public class name
 {
     public static void main(String args[])
     {
-        String s="Bala Omkar";
+        String s="Bala Omkar Surampalli";
         System.out.println(s);
     }
     
